552c8c6585ad86267711a76d0595538782da8857
wasim <wasimxprt@gmail.com>
2018-05-25 16:43:48 +0530

    added gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
 //Gruntfile
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // requirejs compile options
   var compileOptions = {
 
-      mainConfigFile: 'app/scripts/main.js',
-      baseUrl: 'app/scripts',
-      include: ['main'],
-      out: 'dist/main.min.js',
-      removeCombined: false,
-      findNestedDependencies: true,
+    mainConfigFile: 'app/scripts/main.js',
+    baseUrl: 'app/scripts',
+    include: ['main'],
+    out: 'dist/main.min.js',
+    removeCombined: false,
+    findNestedDependencies: true,
 
-      //Removes console.logs for production
-      onBuildWrite: function (moduleName, path, contents) {
-          if(/(.*)js\/modules\/(.*)/.test(path)) return contents.replace(/console.log(.*);/g, ';');
-          return contents;
-      }
+    //Removes console.logs for production
+    onBuildWrite: function (moduleName, path, contents) {
+      if (/(.*)js\/modules\/(.*)/.test(path)) return contents.replace(/console.log(.*);/g, ';');
+      return contents;
+    }
   }
 
   //Initializing the configuration object
@@ -23,50 +23,50 @@ module.exports = function(grunt) {
 
     // Task configuration
     less: {
-        development: {
-            options: {
-              compress: false,  // no minification in dev
-            },
-            files: {
-              //compiling base.less into styles.css
-              "./app/styles/styles.css":"./app/styles/base.less"
-            }
+      development: {
+        options: {
+          compress: false,  // no minification in dev
         },
-        production: {
-          options: {
-            cleancss: true, // minify css
-            // compress: true, // minify css
-          },
-          files: {
-            //compiling base.less into main.min.css
-            "./dist/main.min.css": "./app/styles/base.less"
-          }
+        files: {
+          //compiling base.less into styles.css
+          "./app/styles/styles.css": "./app/styles/base.less"
         }
+      },
+      production: {
+        options: {
+          cleancss: true, // minify css
+          // compress: true, // minify css
+        },
+        files: {
+          //compiling base.less into main.min.css
+          "./dist/main.min.css": "./app/styles/base.less"
+        }
+      }
     },
     requirejs: {
-        compile: {
-            options : compileOptions
-        }
+      compile: {
+        options: compileOptions
+      }
     },
     watch: {
-        less: {
-            // Watch all .less files from the styles directory)
-            files: ['app/styles/*.less'],
-            tasks: ['less'],
-            // Reloads the browser
-            options: {
-              livereload: true  
-            }
-        },
-        requirejs: {
-            // Watch only main.js so that we do not constantly recompile the .js files
-            files: [ 'app/scripts/main.js' ],
-            tasks: [ 'requirejs' ],
-            // Reloads the browser
-            options: {
-              livereload: true  
-            }
+      less: {
+        // Watch all .less files from the styles directory)
+        files: ['app/styles/*.less'],
+        tasks: ['less'],
+        // Reloads the browser
+        options: {
+          livereload: true
         }
+      },
+      requirejs: {
+        // Watch only main.js so that we do not constantly recompile the .js files
+        files: ['app/scripts/main.js'],
+        tasks: ['requirejs'],
+        // Reloads the browser
+        options: {
+          livereload: true
+        }
+      }
     }
   });
 
@@ -76,6 +76,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Task definition
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['less', 'requirejs']);
 
 };
